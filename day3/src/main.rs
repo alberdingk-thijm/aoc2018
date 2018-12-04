bd04d45d0dbd186c9ebca1ec64b3dd545ad2d7c2
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2018-12-03 20:11:44 -0500

    Start day3

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,24 @@
+struct Claim {
+    id: i16,
+    x1: i32,
+    y1: i32,
+    x2: i32,
+    y2: i32,
+}
+
+use std::str::FromStr;
+impl FromStr for Claim {
+    type Err = ();
+    /// Format: "#NNN @ X,Y: XxY"
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Err(())
+    }
+}
+
 fn main() {
+    let input : &'static str = include_str!("../input");
+    let lines = input.lines();
+    // Part 1
+
     println!("Hello, world!");
 }
